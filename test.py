3ec5fd62eb5dda28c64a68817eb4e4b13b045132
Brian Vo <brianvo@Brians-MBP.attlocal.net>
2021-12-19 18:37:38 -0800

    Revert "Added indentation to JSON in Test.py" Unseparating components This reverts commit c59272a88d95677334137b54d93ab1a65b59615a.

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ from api.index import scrape_menu_to_dict
 import json
 
 if __name__=="__main__":
-    with open("eatery_dummy.json", "w", encoding = "utf-8") as f:
-        data = scrape_menu_to_dict("anteatery", date = "01/20/2022")
-        json.dump(data, f, ensure_ascii = False, indent = 4)
-
-    with open("brandy_dummy.json", "w", encoding = "utf-8") as f:
-        data = scrape_menu_to_dict("brandywine", date = "01/20/2022")
-        json.dump(data, f, ensure_ascii = False, indent = 4)
+    with open("eatery_dummy.json","w",encoding="utf-8") as f:
+        data = scrape_menu_to_dict("anteatery",date="01/20/2022")
+        json.dump(data,f,ensure_ascii=False)
+    with open("brandy_dummy.json","w",encoding="utf-8") as f:
+        data = scrape_menu_to_dict("brandywine",date="01/20/2022")
+        json.dump(data,f,ensure_ascii=False)
