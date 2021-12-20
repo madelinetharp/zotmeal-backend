675465124e249ebd6c73f4411cc4d31e0f7824a9
Eric Pedley <ericpedley@gmail.com>
2021-12-20 14:11:09 -0800

    put firebase stuff in separate file

--- api/index.py
+++ api/index.py
@@ -16,28 +16,7 @@ print("Using cache" if USE_CACHE else "Not using cache")
 USE_CACHE = False
 
 if USE_CACHE:
-    #ideally this firebase stuff would be in a separate file but idk how to get vercel to let me import my own files into eachother
-    import firebase_admin#https://firebase.google.com/docs/database/admin/start
-    from firebase_admin import credentials
-    from firebase_admin import db
-    cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_ADMIN_CREDENTIALS")))
-
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': os.getenv("FIREBASE_DATABASE_URL")
-    })
-
-    def get_db_reference(location: str, meal: int, date: str) -> db.Reference:
-        if meal is None:
-            meal = get_current_meal()
-
-        if date is None:
-            irvine_time = get_irvine_time()
-            date = f"{irvine_time.tm_mon}/{irvine_time.tm_mday}/{irvine_time.tm_year}"
-
-        modified_datestring = date.replace("/","|")
-        
-        # .get() returns None if nothing created
-        return db.reference(f"{location}/{modified_datestring}/{meal}")
+    from .firebase_utils import get_db_reference
 
 
 class InvalidQueryException(Exception):
