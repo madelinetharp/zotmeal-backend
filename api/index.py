0923bd42bc13421ac4b600761c8ae35f5c9cee5a
Brian Vo <brianvo@Brians-MBP.attlocal.net>
2021-12-19 21:29:48 -0800

    looks good to me

--- api/index.py
+++ api/index.py
@@ -6,41 +6,39 @@ import traceback#for error handling
 import os
 from datetime import datetime
 from collections import defaultdict
+import requests 
 #anteatery 01/14/2022 breakfast
 #https://uci.campusdish.com/api/menu/GetMenus?locationId=3056&date=01/14/2022&periodId=49
 
-brandy_info = ("Brandywine", "https://uci.campusdish.com/api/menu/GetMenus?locationId=3314")
-eatery_info = ("Anteatery", "https://uci.campusdish.com/api/menu/GetMenus?locationId=3056")
+brandy_info = ('Brandywine', 'https://uci.campusdish.com/api/menu/GetMenus?locationId=3314&periodId={meal_param}&date={date_param}'.format)
+eatery_info = ('Anteatery', 'https://uci.campusdish.com/api/menu/GetMenus?locationId=3056&periodId={meal_param}&date={date_param}'.format)
 
 PROPERTIES = (
-    "IsVegan",
-    "IsVegetarian",
-    "ServingSize",
-    "ServingUnit",
-    "Calories",
-    "CaloriesFromFat",
-    "TotalFat",
-    "TransFat",
-    "Cholesterol",
-    "Sodium",
-    "TotalCarbohydrates",
-    "DietaryFiber",
-    "Sugars",
-    "Protein",
-    "VitaminA",
-    "VitaminC",
-    "Calcium",
-    "Iron",
-    "SaturatedFat"
+    'IsVegan',
+    'IsVegetarian',
+    'ServingSize',
+    'ServingUnit',
+    'Calories',
+    'CaloriesFromFat',
+    'TotalFat',
+    'TransFat',
+    'Cholesterol',
+    'Sodium',
+    'TotalCarbohydrates',
+    'DietaryFiber',
+    'Sugars',
+    'Protein',
+    'VitaminA',
+    'VitaminC',
+    'Calcium',
+    'Iron',
+    'SaturatedFat'
 )
 
 
 ALL_LOCATIONS = {
-    "Brandywine"    : brandy_info,
-    "brandywine"    : brandy_info,
-    "TheAnteatery"  : eatery_info,
-    "Anteatery"     : eatery_info,
-    "anteatery"     : eatery_info
+    'brandywine'    : brandy_info,
+    'anteatery'     : eatery_info
 }
 
 MEAL_IDS = {
@@ -65,6 +63,7 @@ if USE_CACHE:
         'databaseURL': os.getenv("FIREBASE_DATABASE_URL")
     })
 
+
     def get_db_reference(location: str, meal: int, date: str) -> firebase_admin.db.Reference:
         if meal is None:
             meal = get_current_meal()
@@ -73,13 +72,10 @@ if USE_CACHE:
             date = time.strftime("%m/%d/%Y")
 
         modified_datestring = date.replace("/","|")
+        
+        # .get() returns None if nothing created
         return db.reference(f"{location}/{modified_datestring}/{meal}")
-        #for the returned reference, get() returns None when there's nothing created at that path.
 
-def get_irvine_time() -> tuple:#tuple of two ints for hours and minutes
-    '''Return current time in Irvine, PST, by subtracing 8 hours (in seconds) from GMT'''
-    local_time = time.gmtime(time.time() - 28800)
-    return local_time.tm_hour, local_time.tm_min
 
 def get_current_meal():
     '''Return meal code for current time of the day'''
@@ -106,14 +102,17 @@ def get_current_meal():
     if now >= breakfast:
         return 0
 
+
 def uncapitalize_first_letter(s: str) -> str:
-    return s[0].lower()+s[1:]
+    'Capitalize the first letter of a string'
+    return s[0].lower() + s[1:]
+
 
 def scrape_menu_to_dict(location: str, meal_id: int = None, date: str = None) -> dict:
     '''Given a location of a cafeteria, get the corresponding JSON information and 
     return a Python dictionary of the relevant components'''
     
-    restaurant, url = ALL_LOCATIONS[location]
+    restaurant, unformatted_url = ALL_LOCATIONS[location]
 
     if meal_id is None:
         meal_id = get_current_meal()
@@ -121,9 +120,10 @@ def scrape_menu_to_dict(location: str, meal_id: int = None, date: str = None) ->
     if date is None:
         date = time.strftime("%m/%d/%Y")#urllib quote URL-encodes the slashes
 
-    url += f"&periodId={MEAL_IDS[meal_id]}&date={date}"
-    r = urllib.request.urlopen(url)
-    data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
+    data = requests.get(
+            unformatted_url(meal_param = MEAL_IDS[meal_id], date_param = date)
+            ).json()
+
     menu_data = data["Menu"]
     
     final_dict = {
@@ -132,12 +132,11 @@ def scrape_menu_to_dict(location: str, meal_id: int = None, date: str = None) ->
         'all'           : [],
     }
 
-    intermediate_dict = defaultdict(lambda: defaultdict(lambda: []))
+    station_dict        = defaultdict(lambda: defaultdict(lambda: []))
 
-    stations_list = menu_data["MenuStations"]
-    station_id_to_name = dict([(entry['StationId'], entry['Name']) for entry in stations_list])
-
-    products_list = menu_data["MenuProducts"]
+    stations_list       = menu_data["MenuStations"]
+    station_id_to_name  = dict([(entry['StationId'], entry['Name']) for entry in stations_list])
+    products_list       = menu_data["MenuProducts"]
 
 
     def _find_icon(icon_property, food_info):
@@ -159,21 +158,21 @@ def scrape_menu_to_dict(location: str, meal_id: int = None, date: str = None) ->
                 },
         } 
 
-        intermediate_dict[station_name][category_name].append(item_dict)
+        station_dict[station_name][category_name].append(item_dict)
     
-    for station_name in intermediate_dict.keys():
+    for station_name in station_dict.keys():
 
         final_dict["all"].append({
         'station'   : station_name, 
-        'menu'      : [{'category': category, 'items': items} for category, items in intermediate_dict[station_name].items()]
+        'menu'      : [{'category': category, 'items': items} for category, items in station_dict[station_name].items()]
         })
 
     return final_dict
 
-#brandywine lunch 10/14/2021: https://uci.campusdish.com/en/LocationsAndMenus/Brandywine?locationId=3314&storeIds=&mode=Daily&periodId=106&date=10%2F14%2F2021
-#anteatery examples:
-#example url with query (10/14/2021 lunch): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=106&date=10%2F14%2F2021
-#example 2 (10/14/2021 dinner): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=107&date=10%2F14%2F2021
+# brandywine lunch 10/14/2021: https://uci.campusdish.com/en/LocationsAndMenus/Brandywine?locationId=3314&storeIds=&mode=Daily&periodId=106&date=10%2F14%2F2021
+# anteatery examples:
+# example url with query (10/14/2021 lunch): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=106&date=10%2F14%2F2021
+# example 2 (10/14/2021 dinner): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=107&date=10%2F14%2F2021
 # (10/15/2021 dinner): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=107&date=10%2F15%2F2021
 # (10/21/2021 breakfast): https://uci.campusdish.com/en/LocationsAndMenus/TheAnteatery?locationId=3056&storeIds=&mode=Daily&periodId=105&date=10%2F21%2F2021
 
@@ -182,68 +181,128 @@ class InvalidQueryException(Exception):
 class NotFoundException(Exception):
     pass
 
-#to implement redirects see this: https://stackoverflow.com/questions/22701544/redirect-function-with-basehttprequesthandler
-#redirects could be useful to just send the request straight to firebase
+# to implement redirects see this: https://stackoverflow.com/questions/22701544/redirect-function-with-basehttprequesthandler
+# redirects could be useful to just send the request straight to firebase
+
 class handler(BaseHTTPRequestHandler):
+
+    def process_response(self, status_code: int, headers: tuple, data) -> None:
+        ''' 
+        Given a status_code, headers, and data, forward the information to the client
+        '''
+
+        self.send_response(status_code)
+        self.send_header(*headers)
+        self.end_headers()
+        self.wfile.write(data.encode())
+
+    def __read_get(self) -> ('path', 'params', 'query'):
+        '''
+        If a get request is made, validate the path, params, and raw query
+        valid   : return the 3 relevant values in a tuple
+        invalid : raise NotFoundException
+        '''
+        _, _, path, params, raw_query, _ = urllib.parse.urlparse("//"+self.path)#prepending the // separates netloc and path
+
+        if not path == "/api" or path == "/api/":
+            raise NotFoundException
+
+        query = urllib.parse.parse_qs(raw_query)
+
+        return path, params, query
+
+
+    def __validate_query_location(self, query) -> 'location':
+        '''
+        Given a query, verify that a location is present and valid
+        valid   : return the location in lowercase
+        invalid : raise InvalidQueryException
+        '''
+
+        if 'location' not in query:
+            raise InvalidQueryException('No location query parameter specified')
+        
+        location = query['location'][0]
+
+        if location not in ALL_LOCATIONS.keys():
+            raise InvalidQueryException(f'The location specified is not valid. Valid locations: {list(ALL_LOCATIONS.keys())}')
+
+        return location
+
+    def __validate_query_meal_date(self, query) -> ('meal', 'date'):
+        '''
+        Given a query, check if there is a valid combination of meal_id and date
+        valid   : return the two values in a tuple
+        invalid : raise InvalidQueryException
+        '''
+
+        if 'meal' in query:
+            meal = int(query['meal'][0])
+
+        else:
+            meal = None
+
+        if 'date' in query:
+            date = query['date'][0] # note: data gets decoded by urllib, so it will contain slashes.
+
+        else:
+            date = None
+
+        if meal is None and not date is None:
+            raise InvalidQueryException('You can\'t provide the date without the meal (not implemented in the server).')
+
+        return meal, date
+
     def do_GET(self):
+        '''
+        Given a get request for food, respond accordingly
+        '''
+
         try:
-            scheme, netloc, path, params, query, fragment = urllib.parse.urlparse("//"+self.path)#prepending the // separates netloc and path
-            if not path == "/api" or path == "/api/":
-                raise NotFoundException
+            path, params, query = self.__read_get()
+            location            = self.__validate_query_location(query)
+            meal, date          = self.__validate_query_meal_date(query)
 
-            query_params = urllib.parse.parse_qs(query)
+            if USE_CACHE:
+                print(f'date from query params: {date}')
+                db_ref = get_db_reference(location, meal, date)
 
-            try:
-                query_keys = query_params.keys()
-                if not "location" in query_keys:
-                    raise InvalidQueryException("No location query parameter specified")
-                location = query_params["location"][0]
-                if not location in url_dict.keys():#url_dict is in global scope
-                    raise InvalidQueryException(f"The location specified is not valid. Valid locations: {list(url_dict.keys())}")
-                meal=None
-                date=None
-                if "meal" in query_keys:
-                    meal = int(query_params["meal"][0])
-                    if "date" in query_keys:
-                        date = query_params["date"][0]#note: data gets decoded by urllib, so it will contain slashes.
-                elif "date" in query_keys:
-                    raise InvalidQueryException("You can't provide the date without the meal (not implemented in the server). LMK if you think there is a use for providing only the date.")
-                if USE_CACHE:
-                    print(f"date from query params: {date}")
-                    db_ref = get_db_reference(location, meal, date)
-                    db_data = db_ref.get()
-                    if(db_data==None):
-                        data = scrape_menu_to_dict(location, meal, date)
-                        db_ref.set(data)
-                    else:
-                        data = db_data
-                else:
+                db_data = db_ref.get()
+
+                if(db_data is None):
                     data = scrape_menu_to_dict(location, meal, date)
-                
-            except InvalidQueryException as e:
-                self.send_response(400)
-                self.send_header('Content-type','text/plain')
-                self.end_headers()
-                self.wfile.write(f"Invalid query parameters. Details: {e}".encode())
-                return
-            
-            self.send_response(200)
-            self.send_header('Content-type','application/json')
-            self.end_headers()
-            #json.dump(data,self.wfile,ensure_ascii=False) #TODO: this clean solution doesn't work for some reason. says bytes-like is required, not str. figure out why
-            self.wfile.write(json.dumps(data,ensure_ascii=False, indent = 4).encode())
+                    db_ref.set(data)
+
+                else:
+                    data = db_data
+            else:
+                data = scrape_menu_to_dict(location, meal, date)
+                    
+            self.process_response(
+                    status_code = 200, 
+                    headers     = ('Content-type', 'application/json'), 
+                    data        = json.dumps(data, ensure_ascii = False, indent = 4)
+            )
 
         except NotFoundException:
-            self.send_response(404)
-            self.send_header('Content-type','text/plain')
-            self.end_headers()
-            self.wfile.write("Invalid path. The only one available is /api".encode())
+            self.process_response(
+                    status_code = 404, 
+                    headers     = ('Content-type', 'text/plain'), 
+                    data        = 'Invalid path. The only one available is /api'
+            )
 
+        except InvalidQueryException as e:
+            self.process_response(
+                status_code = 400, 
+                headers     = ('Content-type', 'text/plain'), 
+                data        = f'Invalid query parameters. Details: {e}'
+        )
+            
         except Exception as e:
             traceback.print_exc()
-            self.send_response(500)
-            self.send_header('Content-type','text/plain')
-            self.end_headers()
-            self.wfile.write("Internal Server Error. Raise an issue on the github repo: https://github.com/EricPedley/zotmeal-backend".encode())
-
+            self.process_response(
+                    status_code = 500, 
+                    headers     = ('Content-type', 'text/plain'), 
+                    data        = 'Internal Server Error. Raise an issue on the github repo: https://github.com/EricPedley/zotmeal-backend'
+            )
 
