884ca47dd9a833290c8b87b0119d8c52a397f02f
Eric Pedley <ericpedley@gmail.com>
2022-01-03 18:08:55 -0800

    updated testing script

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import json
 
 if __name__=="__main__":
     with open('eatery_dummy.json', 'w', encoding = 'utf-8') as f:
-        data = get_diner_json('anteatery', date = '01/20/2022')
+        data = get_diner_json('anteatery')
         json.dump(data, f, ensure_ascii = False, indent = 4)
 
     with open('brandy_dummy.json', 'w', encoding = 'utf-8') as f:
-        data = get_diner_json('brandywine', date = '01/20/2022')
+        data = get_diner_json('brandywine')
         json.dump(data, f, ensure_ascii = False, indent = 4)
