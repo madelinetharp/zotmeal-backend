58935552773b7d57e03d463d53913388f0a2515f
Eric Pedley <ericpedley@gmail.com>
2022-03-21 12:01:49 -0700

    correctly added event data to diner json

--- api/json_reader.py
+++ api/json_reader.py
@@ -81,7 +81,7 @@ def get_diner_json(location: str, meal_id: int = None, date: str = None) -> dict
         'currentMeal'   : currentMeal,
         'price'         : DEFAULT_PRICES,
         'all'           : [],
-        'themed'        : []
+        'themed'        : [get_event_data(restaurant)]
     }
     print(f'serving request using meal_id {meal_id} and date {date}')
     menu_data = get_menu_data(location, meal_id, date)
@@ -98,7 +98,5 @@ def get_diner_json(location: str, meal_id: int = None, date: str = None) -> dict
                 'menu'      : [{'category': category, 'items': items} for category, items in station_dict[station_name].items()]
             }
         )
-
-    diner_json['themed'].append = get_event_data(restaurant)
 
     return diner_json
