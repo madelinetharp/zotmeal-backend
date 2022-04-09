0a127242fc625530bd52bbb9898160440ffec5ef
RAIL_HU <52292794+RAIL-HU@users.noreply.github.com>
2022-04-09 03:52:18 -0700

    Update location_management.py
    add more style matching cases in align with dining hall webpage updates

--- api/location_management.py
+++ api/location_management.py
@@ -44,7 +44,12 @@ def get_schedule_data(location, date):
                 date_param      = date)
             ).json()['Result']
 
+<<<<<<< Updated upstream
 def get_event_data(restaurant: str) -> dict:
+=======
+def get_event_data(restaurant: str) -> list[dict]:
+    
+>>>>>>> Stashed changes
     '''
     Given a valid location and date,
     perform get request, then parse the HTML code for the event_json using BeautifulSoup 4
@@ -61,9 +66,12 @@ def get_event_data(restaurant: str) -> dict:
                                         'padding: 0.75pt; border-width: 0.75pt; border-right-style: dashed; border-right-color: #999999; border-bottom-style: dashed; border-bottom-color: #999999; text-align: center;',\
                                         'border-right: 0.75pt dashed #999999; border-bottom: 0.75pt dashed #999999; text-align: left;',\
                                         'padding: 0.75pt; border-right: 0.75pt dashed #999999; text-align: left;',\
-                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: left;']})
+                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: left;',\
+                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: center;',\
+                                        'border-right: 0.75pt dashed #999999; border-bottom: 0.75pt dashed #999999; text-align: center;']})
     
-    entries_list = list(map(lambda element: element.getText().strip('\n'), table))
+    entries_list = list(map(lambda element: element.getText().strip('\n').strip(u'\xa0'), table))
+    #print(entries_list)
     rows_list = entries_list[5:]
 
     row = {}
@@ -74,10 +82,31 @@ def get_event_data(restaurant: str) -> dict:
         end_time = normalize_time_from_str(time[1])
         if(curr_time.tm_year > event_date.tm_year or curr_time.tm_yday > event_date.tm_yday or normalize_time(curr_time) > end_time):
             continue
+<<<<<<< Updated upstream
         row['date'] = get_date_str(event_date)
         row['name'] = rows_list[i*4 + 1]
         row['service_start'] = normalize_time_from_str(time[0])
         row['service_end'] = end_time
         break
+=======
+        event_list.append({
+            'date': get_date_str(event_date),
+            'name': rows_list[i+1],
+            'service_start': normalize_time_from_str(start_time),
+            'service_end': normalize_time_from_str(end_time)
+        })
+    if len(event_list) > 0:
+        return event_list
+    else:
+        return  [
+            {
+                "date": "04/20/2069",
+                "name": "placeholder",
+                "service_start": 1100,
+                "service_end": 2200
+            }
+        ]
+
+>>>>>>> Stashed changes
 
     return row
