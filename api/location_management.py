5cef9a2b75f06155db75eeff370d914549bd8623
RAIL_HU <52292794+RAIL-HU@users.noreply.github.com>
2022-04-10 11:40:44 -0700

    Update location_management.py
    add more style matching cases in align with dining hall webpage updates

--- api/location_management.py
+++ api/location_management.py
@@ -45,14 +45,7 @@ def get_schedule_data(location, date):
             ).json()['Result']
 
 def get_event_data(restaurant: str) -> list[dict]:
-    return  [
-            {
-                "date": "04/20/2069",
-                "name": "placeholder",
-                "service_start": 1100,
-                "service_end": 2200
-            }
-        ]
+    
     '''
     Given a valid location and date,
     perform get request, then parse the HTML code for the event_json using BeautifulSoup 4
@@ -69,23 +62,40 @@ def get_event_data(restaurant: str) -> list[dict]:
                                         'padding: 0.75pt; border-width: 0.75pt; border-right-style: dashed; border-right-color: #999999; border-bottom-style: dashed; border-bottom-color: #999999; text-align: center;',\
                                         'border-right: 0.75pt dashed #999999; border-bottom: 0.75pt dashed #999999; text-align: left;',\
                                         'padding: 0.75pt; border-right: 0.75pt dashed #999999; text-align: left;',\
-                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: left;']})
+                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: left;',\
+                                        'padding: 0.75pt; border-bottom: 0.75pt dashed #999999; text-align: center;',\
+                                        'border-right: 0.75pt dashed #999999; border-bottom: 0.75pt dashed #999999; text-align: center;']})
     
-    entries_list = list(map(lambda element: element.getText().strip('\n'), table))
+    entries_list = list(map(lambda element: element.getText().strip('\n').strip(u'\xa0'), table))
+    #print(entries_list)
     rows_list = entries_list[5:]
 
+    row = {}
     curr_time = get_irvine_time()
     event_list = []
-    for i in range(0, len(rows_list), 4):
-        event_date = parse_date(rows_list[i])
-        print(rows_list[i+3])
-        start_time, end_time = rows_list[i + 3].split(' – ')# Warning: this is a weird character. The character U+2013 "–" could be confused with the character U+002d "-", which is more common in source code. UCI uses this weird character in their website for some reason, but if they change it to a normal hyphen this will break.
-        if curr_time>event_date:
+    for i in range(0, int(len(rows_list) / 4)):
+        event_date = parse_date(rows_list[i*4])
+        time = rows_list[i*4 + 3].split(' – ')
+        end_time = normalize_time_from_str(time[1])
+        if(curr_time.tm_year > event_date.tm_year or curr_time.tm_yday > event_date.tm_yday or normalize_time(curr_time) > end_time):
             continue
         event_list.append({
             'date': get_date_str(event_date),
-            'name': rows_list[i+1],
-            'service_start': normalize_time_from_str(start_time),
-            'service_end': normalize_time_from_str(end_time)
+            'name': rows_list[i*4+1],
+            'service_start': normalize_time_from_str(time[0]),
+            'service_end': end_time
         })
+    if len(event_list) > 0:
+        return event_list
+    else:
+        return  [
+            {
+                "date": "04/20/2069",
+                "name": "placeholder",
+                "service_start": 1100,
+                "service_end": 2200
+            }
+        ]
 
+
+    return row
