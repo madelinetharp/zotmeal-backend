f08a2022fc3d82e939403b6ac98d16851d29691a
Eric Pedley <ericpedley@gmail.com>
2023-02-06 13:03:23 -0800

    remove everything else

--- local_server.py
+++
@@ -1,4 +0,0 @@
-from http.server import HTTPServer
-from api.index import handler
-
-HTTPServer(("",3000),handler).serve_forever()
--- test.py
+++
@@ -1,5 +0,0 @@
-import json
-from api.campusdish_interface import get_menu_data
-
-with open("eaterytest.json", "w") as f:
-    json.dump(get_menu_data("anteatery"),f)
