b6d41234ca5159f2e240b0ff63385920371ff23c
Nathan Ng <nathan@dhcp-v063-116.mobile.uci.edu>
2023-02-08 14:49:01 -0800

    added analytics updater

--- local_server.py
+++ local_server.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 from http.server import HTTPServer
 from api.index import handler
+
+
+
 
 HTTPServer(("",3000),handler).serve_forever()
